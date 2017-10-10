053d2f7e5afaea4a160bf51d352c452bae0c5c23
blue shadow <anujose1989@gmail.com>
2017-10-10 06:05:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,41 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  item[itemPrice]= Math.floor(Math.random()*100);
+  console.log(`${item.itemName} has been added to your cart.`);
+  return Object.assign({}, cart, { item.itemName :   item.itemPrice });
 }
 
 function viewCart() {
-  // write your code here
+    if(cart.length ===0){
+      console.log("Your shopping cart is empty.");
+    }
+    else{
+      var text = "In your cart, you have";
+      for (var i = 0 ; i < cart.length ; i++) {
+            text += `${item.itemName} at ${item.itemPrice}` ;
+      }
+    }
 }
 
 function total() {
-  // write your code here
+  let sum = 0;
+  for (var i of cart) {
+    sum = sum + i[itemPrice];
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  delete cart.itemName;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if ( cardNumber == undefined){
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+  else{
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = nil;
+  }
 }
